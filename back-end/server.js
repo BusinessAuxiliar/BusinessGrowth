7f892be63eee2ef9fee1b6027358165d35fc5b9d
BusinessAuxiliar <business.sistema.auxiliar@gmail.com>
2025-04-11 08:27:29 -0300

    formatDocument

--- back-end/server.js
+++ back-end/server.js
@@ -1,41 +1,41 @@
-import express from 'express';
-import cors from 'cors';
-import { getUserByUsername } from './app.js';
+import express from "express";
+import cors from "cors";
+import { getUserByUsername } from "./app.js";
 
 const app = express();
-app.use(cors({ origin: '*', methods: ['POST', 'GET'], credentials: true }));
-app.use(express.json());
 const port = 3001;
-app.post('/sign-in', async (req, res) => {
+app.use(cors({ origin: "*", methods: ["POST", "GET"], credentials: true }));
+app.use(express.json());
+
+app.post("/sign-in", async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
-    return res.status(400).json({ message: 'Faltan datos' });
+    return res.status(400).json({ message: "Faltan datos" });
   }
 
   try {
     const user = await getUserByUsername(username);
 
     if (!user) {
-      return res.status(401).json({ message: 'Usuario no encontrado' });
+      return res.status(401).json({ message: "Usuario no encontrado" });
     }
 
     if (user.usu_password === password) {
       return res.status(200).json({
-        message: '✅ Login exitoso',
+        message: "✅ Login exitoso",
         userId: user.usu_id,
-        username: user.usu_nombre
+        username: user.usu_nombre,
       });
     } else {
-      return res.status(401).json({ message: 'Contraseña incorrecta' });
+      return res.status(401).json({ message: "Contraseña incorrecta" });
     }
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Error interno del servidor' });
+    res.status(500).json({ message: "Error interno del servidor" });
   }
 });
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
-  });
-  
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
